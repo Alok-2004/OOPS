0cc7f046ed46e6763e50a4a699ccad428ab51028
Alok prasad <118293432+Alok-2004@users.noreply.github.com>
2023-08-28 23:23:44 +0530

    Done

---
+++ Pointers/Dangling_pointer_example1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int *ptr;
+   {
+       int v=23;
+       ptr = &v;
+       cout<<"Value is(inside block):"<<*ptr<<"\n";
+       cout<<"Address is(inside block):"<<ptr<<"\n";
+   }
+   // Here ptr is dangling pointer as v is no longer existing
+	cout<<"Value is(outside block):"<<*ptr<<"\n";
+	cout<<"Address is(outside block):"<<ptr;//ptr is dangling pointer(same address)
+    ptr=NULL;//Solution to dangling pointer(assign null address)
+
+}
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string nums;
+	cin>>nums;
+	
+	int dec = 0;
+	
+	for(int i = 0; i<str.size(); i++)
+	{
+		
+	}
+}
